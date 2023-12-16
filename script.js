8fd833aa77c90dd4abdc6129bd8bd9f43961bb7b
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-12-16 21:53:42 +0600

    add icon and functioning

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const themeToggle = document.getElementById('theme-toggle');
+  const body = document.body;
+
+  themeToggle.addEventListener('click', function () {
+      body.classList.toggle('dark-theme');
+  });
+});
